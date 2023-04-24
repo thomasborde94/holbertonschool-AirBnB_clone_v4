aa4bbde7403df01f72d54cdf952aecd75eac1698
Thomas Borde <thomas.borde94@hotmail.fr>
2023-04-24 04:50:30 -0700

    Added check option to the list of amenities

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  let amenities = {};
+
+  $('input[type="checkbox"]').change(function() {
+    if (this.checked) {
+      amenities[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenities[$(this).data('id')];
+    }
+    let amenitiesList = Object.values(amenities).join(', ');
+    $('div.amenities h4').text(amenitiesList);
+  });
+});
